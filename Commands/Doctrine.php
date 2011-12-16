ca8d143b13f23d011c158e7fb37d95c37db74efc
jasir <sirjarda2@gmail.com>
2011-12-16 11:34:39 +0100

    minor

--- Commands/Doctrine.php
+++ Commands/Doctrine.php
@@ -9,41 +9,27 @@ class Doctrine {
 	/* --- Public API --- */
 
 	public static function register(\Panels\CommandPanel\Panel $panel, $group = 'Doctrine') {
-		
-		$panel->addCommand($group, 
-				Command::create(
-					'Create Schema',
-					function ($container) {
-						$em=$container->entityManager;
-						$schemaTool = new \Doctrine\ORM\Tools\SchemaTool($em);
-						$metadatas = $em->getMetadataFactory()->getAllMetadata();
-						$schemaTool->createSchema($metadatas);
-					}
-				)
-		);
 
-		$panel->addCommand($group, 
-				Command::create(
-					'Update Schema',
-					function ($container) {
-						$em=$container->entityManager;
-						$schemaTool = new \Doctrine\ORM\Tools\SchemaTool($em);
-						$metadatas = $em->getMetadataFactory()->getAllMetadata();
-						$schemaTool->updateSchema($metadatas);
-					}
-				)
-		);
+		$panel->addCommand($group, Command::create('Update Schema',function ($container) {
+			$em=$container->entityManager;
+			$schemaTool = new \Doctrine\ORM\Tools\SchemaTool($em);
+			$metadatas = $em->getMetadataFactory()->getAllMetadata();
+			$schemaTool->updateSchema($metadatas);
+		}));
 
-		$panel->addCommand($group, 
-				Command::create(
-					'Drop Schema',
-					function ($container) {
-						$em=$container->entityManager;
-						$schemaTool = new \Doctrine\ORM\Tools\SchemaTool($em);
-						$metadatas = $em->getMetadataFactory()->getAllMetadata();
-						$schemaTool->dropSchema($metadatas);
-					}
-				)
-		);
+		$panel->addCommand($group, Command::create('Create Schema', function ($container) {
+			$em=$container->entityManager;
+			$schemaTool = new \Doctrine\ORM\Tools\SchemaTool($em);
+			$metadatas = $em->getMetadataFactory()->getAllMetadata();
+			$schemaTool->createSchema($metadatas);
+		}));
+
+		$panel->addCommand($group, Command::create('Drop Schema',function ($container) {
+			$em=$container->entityManager;
+			$schemaTool = new \Doctrine\ORM\Tools\SchemaTool($em);
+			$metadatas = $em->getMetadataFactory()->getAllMetadata();
+			$schemaTool->dropSchema($metadatas);
+		}));
+
 	}
 }
